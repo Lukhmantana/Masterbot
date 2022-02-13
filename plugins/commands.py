675062c3fb9a5cb8379ef977639bddfce96774e5
Lukhmanvp <88678514+Lukhmanvp@users.noreply.github.com>
2022-02-13 17:41:04 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -47,7 +47,7 @@ async def start(client, message):
             InlineKeyboardButton('ᴏᴡɴᴇʀ', url='https://t.me/lukhman_jr'),
             InlineKeyboardButton('ɢʀᴏᴜᴘ', url='https://t.me/AM_chats_2_0')
             ],[
-            InlineKeyboardButton('ᴄʜᴀɴɴᴇʟ', url='https://t.me/MOLLYWO'),
+            InlineKeyboardButton('ᴄʜᴀɴɴᴇʟ', url='https://t.me/cinmeamax'),
             InlineKeyboardButton('ᴄʟᴏꜱᴇ', callback_data='close_data')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
